cca40ba36b15ae0b2427eaf78ae7fd1c0e724c2e
JulianAvella <julianavella01@gmail.com>
2026-08-11 10:38:46 -0500

    feat: Se actualiza la version de angular 19 a 20. Refs udistrital/atlas_externo_mf#3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { CaptchaGateComponent } from './components/captcha-gate/captcha-gate.component';
+//import { CaptchaGateComponent } from './components/captcha-gate/captcha-gate.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, HeaderComponent, FooterComponent, CaptchaGateComponent],
+    imports: [RouterOutlet, HeaderComponent, FooterComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
